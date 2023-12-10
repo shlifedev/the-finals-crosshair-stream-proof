f82b7cf220afa4d0d180a7af152a6040783048f8
shlifedev <shlifedev@gmail.com>
2023-12-10 09:39:15 +0900

    crosshair 1920x1080

---
+++ crosshair/src/main.rs
@@ -0,0 +1,228 @@
+#![allow(dead_code)]
+#![feature(const_fn_floating_point_arithmetic)]
+
+use std::{
+    cell::{
+        RefCell,
+        RefMut,
+    },
+    error::Error,
+    fmt::Debug,
+    fs::File,
+    io::BufWriter,
+    path::PathBuf,
+    rc::Rc,
+    sync::{
+        atomic::{
+            AtomicBool,
+            Ordering,
+        },
+        Arc,
+    },
+    time::{
+        Duration,
+        Instant,
+    }, thread::{sleep, sleep_ms},
+};
+
+use anyhow::{Context, Ok};
+use clap::{
+    Args,
+    Parser,
+    Subcommand,
+};
+use imgui::{
+    Condition,
+    FontConfig,
+    FontId,
+    FontSource,
+    Ui, sys::{ImVec2_ImVec2_Float, ImColor_ImColor_Float, ImColor, ImVec4},
+};
+
+use overlay::{
+    LoadingError,
+    OverlayError,
+    OverlayOptions,
+    OverlayTarget,
+    SystemRuntimeController,
+};
+
+enum AppCommand {
+    /// Start the overlay
+    Overlay,
+
+    /// Create a schema dump
+    DumpSchema(SchemaDumpArgs),
+}
+
+
+pub struct AppFonts {
+    valthrun: FontId,
+}
+
+use obfstr::obfstr;
+
+use native_dialog::{FileDialog, MessageDialog, MessageType};
+
+pub struct Application {
+
+}
+
+impl Application {
+    pub fn pre_update(&mut self, controller: &mut SystemRuntimeController) -> anyhow::Result<()> {
+      
+
+      
+      controller.toggle_screen_capture_visibility(false);
+        Ok(())
+    }
+
+    pub fn update(&mut self, ui: &imgui::Ui) -> anyhow::Result<()> {
+        {
+
+        } 
+
+
+        Ok(())
+    }
+
+
+    fn render_overlay(&self, ui: &imgui::Ui) {
+
+        let text_buf;
+        let text = obfstr!(text_buf = "Valthrun Overlay");
+        
+        let mut x = 398.0;
+        let mut y = 222.0;
+        let mut size = 3.0;
+        let mut border = 1.0;
+        ui.get_window_draw_list().add_rect([x-border,y-border], [x+size+border, y+size+border], [0.0,0.0,0.0]).filled(true).build();
+
+         ui.get_window_draw_list().add_rect([x,y], [x+size, y+size], [0.0,255.0,0.0]).filled(true).build();
+
+    }
+    
+    pub fn render(&self, ui: &imgui::Ui) {
+        ui.window("overlay")
+        .draw_background(false)
+        .no_decoration()
+        .no_inputs()
+        .size(ui.io().display_size, Condition::Always)
+        .position([0.0, 0.0], Condition::Always)
+        .build(|| self.render_overlay(ui));
+ 
+
+    }
+
+
+}
+
+struct SchemaDumpArgs {
+    pub target_file: PathBuf,
+}
+
+
+fn main(){
+ 
+
+    let cmd : Option<AppCommand> = Default::default();
+    let command = cmd.as_ref().unwrap_or(&AppCommand::Overlay);
+    let result = match command {
+        AppCommand::DumpSchema(args) => {},
+        AppCommand::Overlay => main_overlay().expect("")
+    };
+
+
+}
+
+fn main_overlay() -> anyhow::Result<()> {
+
+
+ 
+
+    let title = String::from("THE FINALS");
+    let app_fonts: Rc<RefCell<Option<AppFonts>>> = Default::default();
+    let overlay_options = OverlayOptions {
+        title: obfstr!("CS2 Overlay").to_string(),
+        target: OverlayTarget::WindowTitle(title),
+        font_init: Some(Box::new({
+            let app_fonts = app_fonts.clone();
+
+            move |imgui| {
+                let mut app_fonts = app_fonts.borrow_mut();
+
+                let font_size = 18.0;
+                let valthrun_font = imgui.fonts().add_font(&[FontSource::TtfData {
+                    data: include_bytes!("../resources/Valthrun-Regular.ttf"),
+                    size_pixels: font_size,
+                    config: Some(FontConfig {
+                        rasterizer_multiply: 1.5,
+                        oversample_h: 4,
+                        oversample_v: 4,
+                        ..FontConfig::default()
+                    }),
+                }]);
+
+                *app_fonts = Some(AppFonts {
+                    valthrun: valthrun_font,
+                });
+            }
+        })),
+    };
+
+    let app = Application{
+
+    };
+    let app = Rc::new(RefCell::new(app));
+    let overlay = match overlay::init(&overlay_options) {
+        value => value?,
+    };
+
+
+    let mut update_fail_count = 0;
+    let mut update_timeout: Option<(Instant, Duration)> = None;
+
+
+ overlay.main_loop({
+    let app = app.clone();
+                       move |controller| {
+                           let mut app = app.borrow_mut();
+                           if let Err(_err) = app.pre_update(controller) {
+                               false
+                           } else {
+                               true
+                           }
+                       }
+ }, move |ui| {
+     let mut app = app.borrow_mut();
+
+
+     if let Some((timeout, target)) = &update_timeout {
+        if timeout.elapsed() > *target {
+            update_timeout = None;
+        } else {
+            /* Not updating. On timeout... */
+            return true;
+        }
+    }
+
+
+
+     if let Err(err) = app.update(ui) {
+         if update_fail_count >= 10 {
+             log::error!("Over 10 errors occurred. Waiting 1s and try again.");
+             log::error!("Last error: {:#}", err);
+
+             update_timeout = Some((Instant::now(), Duration::from_millis(1000)));
+             update_fail_count = 0;
+             return true;
+         } else {
+             update_fail_count += 1;
+         }
+     }
+
+     app.render(ui);
+     true
+ }
+ );
+}
---
+++ overlay/examples/taskmgr_overlay.rs
@@ -0,0 +1,59 @@
+use imgui::{
+    FontConfig,
+    FontGlyphRanges,
+    FontSource,
+};
+use overlay::OverlayTarget;
+
+fn main() -> anyhow::Result<()> {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Trace)
+        .parse_default_env()
+        .init();
+
+    log::info!("Initialize overlay");
+    let overlay = overlay::init(&overlay::OverlayOptions {
+        title: "Task Manager Overlay".to_string(),
+        target: OverlayTarget::WindowTitle("Task Manager".into()),
+        font_init: Some(Box::new(|imgui| {
+            // imgui.fonts().add_font(font_sources)
+            // imgui.fonts().add_font(&[FontSource::TtfData {
+            //     data: include_bytes!("../resources/unifont-15.1.03.otf"),
+            //     size_pixels: 16.0,
+            //     config: Some(FontConfig {
+            //         glyph_ranges: FontGlyphRanges::from_slice(&[0x0001, 0xFFFF, 0x0000]),
+            //         ..FontConfig::default()
+            //     }),
+            // }]);
+        })),
+    })?;
+    let mut text_input = Default::default();
+    overlay.main_loop(
+        |controller| {
+            controller.toggle_debug_overlay(true);
+            true
+        },
+        move |ui| {
+            ui.window("Dummy Window")
+                .resizable(true)
+                .movable(true)
+                .build(|| {
+                    ui.text("Taskmanager Overlay!");
+                    ui.text(format!("FPS: {:.2}", ui.io().framerate));
+                    ui.input_text("Test-Input", &mut text_input).build();
+
+                    ui.text("Привет, мир!");
+                    ui.text("Chào thế giới!");
+                    ui.text("Chào thế giới!");
+                    ui.text("ສະ​ບາຍ​ດີ​ຊາວ​ໂລກ!");
+                    ui.text("Салом Ҷаҳон!");
+                    ui.text("こんにちは世界!");
+                    ui.text("你好世界!");
+                    ui.text("﷽, ♛ LAZ ♛,  ♛ ॐ,  ♛ ॐ");
+                    ui.text(" ♣▄♠░ ");
+                    ui.text("♣♠░:D ︻デ── ");
+                });
+            true
+        },
+    );
+}
